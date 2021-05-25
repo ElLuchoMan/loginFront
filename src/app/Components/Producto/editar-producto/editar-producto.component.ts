b682b5266a221d5244a5742b3a287c136af273a4
Bryan Luis <baluist@correo.udistrital.edu.co>
2021-05-25 09:54:06 -0500

    Agregado editar

--- src/app/Components/Producto/editar-producto/editar-producto.component.ts
+++ src/app/Components/Producto/editar-producto/editar-producto.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductoService } from 'src/app/Services/producto.service';
 
 @Component({
   selector: 'app-editar-producto',
@@ -7,9 +9,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditarProductoComponent implements OnInit {
 
-  constructor() { }
+  form: any = {};
+  actualizado = false;
+  failActualizado = false;
+  msjErr = '';
+  msjOK = '';
 
-  ngOnInit(): void {
+  failInit = false;
+
+  constructor(private productoService: ProductoService,    private activatedRoute: ActivatedRoute,    private router: Router) { }
+
+  ngOnInit() {
+    const id = this.activatedRoute.snapshot.params.id;
+    this.productoService.detalle(id).subscribe( data => {
+      this.form.nombreProducto = data.nombreProducto;
+      this.form.precio = data.precio;
+    },
+      (err: any) => {
+        this.failInit = true;
+        this.router.navigate(['']);
+      }
+    );
   }
 
+  onUpdate(): void {
+    const id = this.activatedRoute.snapshot.params.id;
+    this.productoService.editar(this.form, id).subscribe( data => {
+      this.actualizado = true;
+      this.failActualizado = false;
+      this.msjOK = data.mensaje;
+    },
+    (err: any) => {
+      this.actualizado = false;
+      this.failActualizado = true;
+      this.msjErr = err.error.mensaje;
+    }
+    );
+  }
+
+  volver(): void {
+    window.history.back();
+  }
 }
