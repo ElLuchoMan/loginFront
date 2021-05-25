2e9306266e2413060021cd968a8e22bd8d08460c
Bryan Luis <baluist@correo.udistrital.edu.co>
2021-05-25 12:57:48 -0500

    Se agrega guard

---
+++ src/app/Guards/guard.guard.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { TokenService } from '../Services/token.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GuardGuard implements CanActivate {
+  realRol: string;
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    const expectedRol = route.data.expectedRol;
+    const roles = this.tokenService.getAuthorities();
+    this.realRol = 'user';
+    roles.forEach(rol => {
+      if (rol === 'ROLE_ADMIN') {
+        this.realRol = 'admin';
+      }
+    });
+    if (!this.tokenService.getToken() || expectedRol.indexOf(this.realRol) === -1) {
+      this.router.navigate(['']);
+      return false;
+    }
+    return true;
+  }
+
+  constructor(private tokenService: TokenService, private router: Router) { }
+}
