f3d4799a6daba00ccead41d5880b2efd0c16e5b6
Bryan Luis <baluist@correo.udistrital.edu.co>
2021-05-25 12:34:43 -0500

    Fixed auth service

--- src/app/Services/auth.service.ts
+++ src/app/Services/auth.service.ts
@@ -1,15 +1,17 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { JwtModel } from '../Models/jwt-model.interface';
 import { LoginUsuario } from '../Models/login-usuario.interface';
+import { NuevoUsuario } from '../Models/nuevo-usuario.interface';
 
+const headers = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
 @Injectable({
   providedIn: 'root'
 })
-const headers = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
 export class AuthService {
 
-  private authURL = 'http://localhost:8080/api/auth/;;
+  private authURL = 'http://localhost:8080/api/auth/';
 
   constructor(private httpClient: HttpClient) { }
 
