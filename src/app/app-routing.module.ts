c7c0b35bc7744b2989bc754dff6e86f88ebd08f0
Bryan Luis <baluist@correo.udistrital.edu.co>
2021-05-25 12:49:03 -0500

    Se agrega verificador de sesión

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './Components/Home/home.component';
+import { LoginComponent } from './Components/login/login.component';
 import { DetalleProductoComponent } from './Components/Producto/detalle-producto/detalle-producto.component';
 import { EditarProductoComponent } from './Components/Producto/editar-producto/editar-producto.component';
 import { ListarProductoComponent } from './Components/Producto/listar-producto/listar-producto.component';
@@ -8,13 +9,15 @@ import { NuevoProductoComponent } from './Components/Producto/nuevo-producto/nue
 
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'lista', component: ListarProductoComponent },
-  { path: 'detalle/:id', component: DetalleProductoComponent },
-  { path: 'nuevo', component: NuevoProductoComponent },
-  { path: 'editar/:id', component: EditarProductoComponent },
-  { path: '**', redirectTo: 'home', pathMatch: 'full' }
+  {path: '', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'lista', component: ListarProductoComponent},
+  {path: 'detalle/:id', component: DetalleProductoComponent},
+  {path: 'nuevo', component: NuevoProductoComponent},
+  {path: 'editar/:id', component: EditarProductoComponent},
+  // rutas a login y registro
+  {path: 'login', component: LoginComponent},
+  {path: '**', redirectTo: 'home', pathMatch: 'full'}
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { TokenService } from './Services/token.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'loginFront';
+  isLogin = false;
+  roles: string[];
+  authority: string;
+  constructor(private tokenService: TokenService, private router: Router) { }
+
+  ngOnInit() {
+    if (this.tokenService.getToken()) {
+      this.isLogin = true;
+      this.roles = [];
+      this.roles = this.tokenService.getAuthorities();
+      this.roles.every(rol => {
+        if (rol === 'ROLE_ADMIN') {
+          this.authority = 'admin';
+          return false;
+        }
+        this.authority = 'user';
+        return true;
+      });
+    }
+  }
+
+  logOut(): void {
+    this.tokenService.logOut();
+    this.isLogin = false;
+    this.authority = '';
+    this.router.navigate(['home']);
+  }
 }
